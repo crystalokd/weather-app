ebae6096b20ed10d9dff62a62e06b6ce07766f90
crystal <crystalokd@gmail.com>
2022-09-15 17:43:31 +0100

    ....1

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '31979771c0mshf018f558007c40ep11481ejsnb5bf12ec993e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
